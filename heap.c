a38fb8219a3133e9091f9ad9aaf3e28bf8c315ae
Replit user <>
2023-10-15 20:17:14 +0000

    new try

--- heap.c
+++ heap.c
@@ -34,9 +34,9 @@ void heap_push(Heap* pq, void* data, int priority){
     padre=pq->heapArray[(nuevoD-1)/2];
     if(padre.priority<=pq->heapArray[nuevoD].priority)break;
     aux=padre;
-    pq->heapArray[(nuevoD-1)/2]=pq->heapArray[nuevoD];
+    pq->heapArray[(nuevoD-1)]=pq->heapArray[nuevoD];
     pq->heapArray[nuevoD]=aux;
-    nuevoD = (nuevoD-1);
+    nuevoD = (nuevoD-1)/2;
     
   }
 }
